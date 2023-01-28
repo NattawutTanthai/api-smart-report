8067d10d86c01f8b2a9229cec28ae59b0dfbe1c6
NattawutTanthai <erictyth@gmail.com>
2023-01-28 21:01:21 +0700

    del vercel file

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send({ message: "Smart Report API" });
+    res.send({ message: "Powered by Smart Report." });
   });
 
 module.exports = app;
