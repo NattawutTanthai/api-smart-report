f782d7b8fd035ec0a9271021747715f25b3108c6
NattawutTanthai <erictyth@gmail.com>
2023-01-28 22:31:50 +0700

    test index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const router = express.Router();
+const type = require("../schemas/type");
 const app = express();
-
 app.get("/", (req, res) => {
-    res.send({ message: "Powered by Smart Report." });
+    // res.send({ message: "Powered by Smart Report." });
+    type.find((err, type) => {
+        if (err) return next(err);
+        res.json(type);
+      });
   });
 
 module.exports = app;
