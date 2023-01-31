f3243c7a7058191c9c5223505b15fb6a494435ef
NattawutTanthai <erictyth@gmail.com>
2023-02-01 00:30:40 +0700

    add fix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 const express = require("express");
-const router = express.Router();
-const type = require("../schemas/type");
 const app = express();
+
 app.get("/", (req, res) => {
-    // res.send({ message: "Powered by Smart Report." });
-    type.find((err, type) => {
-        if (err) return next(err);
-        res.json(type);
-      });
+    res.send({ message: "Powered by Smart Report." });
   });
 
 module.exports = app;
